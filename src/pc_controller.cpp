342853eee19a3b2a7471a240d41d38a801e4d406
d3fuse99 <d3fuse99@gmail.com>
2026-08-14 13:56:30 +0300

    add: src/pc_controller.cpp

---
+++ src/pc_controller.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include "pc_controller.h"
+
+BleKeyboard bleKeyboard("StickZero OS", "M5Stack", 100);
+
+void initPcController() {
+    bleKeyboard.begin();
+}
+
+void pcShutdownBle() {
+    if (!bleKeyboard.isConnected()) return;
+    bleKeyboard.press(KEY_LEFT_GUI);
+    bleKeyboard.press('x');
+    delay(300);
+    bleKeyboard.releaseAll();
+    delay(300);
+    bleKeyboard.write('u');
+    delay(300);
+    bleKeyboard.write('u');
+}
+
+void pcLockScreen() {
+    if (!bleKeyboard.isConnected()) return;
+    bleKeyboard.press(KEY_LEFT_GUI);
+    bleKeyboard.press('l');
+    delay(100);
+    bleKeyboard.releaseAll();
+}
