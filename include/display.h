bf1cc26dea8d77dafb26a803e91566ddd2505521
d3fuse99 <d3fuse99@gmail.com>
2026-08-14 13:56:29 +0300

    add: include/display.h

---
+++ include/display.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <M5StickCPlus.h>
+
+enum MenuState {
+    MENU_MAIN,
+    MENU_PC,
+    MENU_TV,
+    MENU_AC
+};
+
+extern MenuState currentMenu;
+extern int selectedIndex;
+
+void initDisplay();
+void renderUI();
+void showLoadingAnimation(const char* title);
+void showStatus(const char* msg, uint16_t bgColor, uint16_t fgColor);
+int getBatteryPercent();
+void setPowerSave(bool enable);
