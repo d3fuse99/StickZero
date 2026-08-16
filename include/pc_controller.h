c295c7d55dbbc309aab38c7e6d8584739878a0e5
d3fuse99 <d3fuse99@gmail.com>
2026-08-16 21:44:29 +0300

    update: include/pc_controller.h

--- include/pc_controller.h
+++ include/pc_controller.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <Arduino.h>
 #include <BleKeyboard.h>
 
 extern BleKeyboard bleKeyboard;
 void initPcController();
 void pcShutdownBle();
 void pcLockScreen();
+void pcMuteAudio();
