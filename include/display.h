4694d7f3c1ebf64575482d01fae881964c27e9eb
d3fuse99 <d3fuse99@gmail.com>
2026-08-16 21:44:29 +0300

    update: include/display.h

--- include/display.h
+++ include/display.h
@@ -17,3 +17,4 @@ void showLoadingAnimation(const char* title);
 void showStatus(const char* msg, uint16_t bgColor, uint16_t fgColor);
 int getBatteryPercent();
 void setPowerSave(bool enable);
+void startFlashlight();
