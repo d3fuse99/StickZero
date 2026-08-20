5aa666b96a600285b75988cba2ed98508090953c
d3fuse99 <d3fuse99@gmail.com>
2026-08-20 20:38:33 +0300

    feat: implement StickZero OS v5.0 and update src/beaver.cpp

--- src/beaver.cpp
+++ src/beaver.cpp
@@ -1,108 +1,60 @@
 #include "beaver.h"
 
-enum BeaverMood {
-    BOBER_IDLE,
-    BOBER_HAPPY,
-    BOBER_CHOP,
-    BOBER_SLEEP
-};
-
-static int boberLevel = 1;
-static int logsChopped = 0;
-
-void drawPixelBeaver(int x, int y, int frame, BeaverMood mood) {
+void drawDetailedBeaver(int x, int y, int frame) {
     uint16_t brown = 0x8A22;
-    uint16_t darkBrown = 0x4A00;
+    uint16_t darkBrown = 0x4200;
     uint16_t earColor = 0xD4C8;
 
-    M5.Lcd.fillRect(x + 10, y + 10, 40, 45, brown);
-    M5.Lcd.fillRect(x + 15, y + 4, 30, 15, brown);
+    int tailWag = (frame % 2 == 0) ? 0 : 3;
+    M5.Lcd.fillRect(x + 55, y + 25 + tailWag, 28, 16, darkBrown);
+    M5.Lcd.drawRect(x + 55, y + 25 + tailWag, 28, 16, BLACK);
+    M5.Lcd.drawLine(x + 60, y + 27 + tailWag, x + 78, y + 38 + tailWag, BLACK);
+    M5.Lcd.drawLine(x + 60, y + 38 + tailWag, x + 78, y + 27 + tailWag, BLACK);
 
-    M5.Lcd.fillCircle(x + 16, y + 4, 5, darkBrown);
-    M5.Lcd.fillCircle(x + 44, y + 4, 5, darkBrown);
-    M5.Lcd.fillCircle(x + 16, y + 4, 3, earColor);
-    M5.Lcd.fillCircle(x + 44, y + 4, 3, earColor);
+    M5.Lcd.fillRect(x + 12, y + 10, 48, 48, brown);
+    M5.Lcd.fillRect(x + 18, y + 2, 36, 16, brown);
 
-    int tailWag = (frame % 2 == 0) ? 0 : 4;
-    M5.Lcd.fillRect(x + 48, y + 35 + tailWag, 25, 14, darkBrown);
-    M5.Lcd.drawRect(x + 48, y + 35 + tailWag, 25, 14, BLACK);
+    M5.Lcd.fillCircle(x + 18, y + 2, 6, darkBrown);
+    M5.Lcd.fillCircle(x + 54, y + 2, 6, darkBrown);
+    M5.Lcd.fillCircle(x + 18, y + 2, 3, earColor);
+    M5.Lcd.fillCircle(x + 54, y + 2, 3, earColor);
 
-    if (mood == BOBER_SLEEP) {
-        M5.Lcd.drawFastHLine(x + 22, y + 16, 6, BLACK);
-        M5.Lcd.drawFastHLine(x + 32, y + 16, 6, BLACK);
-        M5.Lcd.setTextColor(CYAN, BLACK);
-        M5.Lcd.setTextSize(1);
-        M5.Lcd.setCursor(x + 55, y - 5);
-        M5.Lcd.print("Zzz...");
-    } else {
-        M5.Lcd.fillRect(x + 20, y + 12, 8, 8, CYAN);
-        M5.Lcd.fillRect(x + 32, y + 12, 8, 8, CYAN);
-        M5.Lcd.fillRect(x + 23, y + 14, 3, 4, BLACK);
-        M5.Lcd.fillRect(x + 35, y + 14, 3, 4, BLACK);
-    }
+    M5.Lcd.fillRect(x + 20, y + 12, 32, 10, CYAN);
+    M5.Lcd.drawRect(x + 20, y + 12, 32, 10, WHITE);
+    M5.Lcd.fillRect(x + 26, y + 14, 5, 6, BLACK);
+    M5.Lcd.fillRect(x + 41, y + 14, 5, 6, BLACK);
 
-    M5.Lcd.fillCircle(x + 30, y + 25, 4, BLACK);
-    M5.Lcd.fillRect(x + 26, y + 30, 8, 8, WHITE);
-    M5.Lcd.drawFastVLine(x + 30, y + 30, 8, BLACK);
+    M5.Lcd.fillCircle(x + 36, y + 28, 4, BLACK);
 
-    M5.Lcd.fillCircle(x + 16, y + 54, 5, darkBrown);
-    M5.Lcd.fillCircle(x + 44, y + 54, 5, darkBrown);
+    M5.Lcd.fillRect(x + 31, y + 34, 10, 10, WHITE);
+    M5.Lcd.drawRect(x + 31, y + 34, 10, 10, BLACK);
+    M5.Lcd.drawFastVLine(x + 36, y + 34, 10, BLACK);
 
-    if (mood == BOBER_CHOP) {
-        M5.Lcd.fillRect(x - 15, y + 25, 12, 28, 0xC400);
-        M5.Lcd.fillRect(x - 20, y + 20, 22, 10, 0x8410);
-    }
+    M5.Lcd.fillCircle(x + 20, y + 58, 6, darkBrown);
+    M5.Lcd.fillCircle(x + 52, y + 58, 6, darkBrown);
 }
 
-void startBeaverCompanion() {
-    int frame = 0;
-    BeaverMood mood = BOBER_IDLE;
-    uint32_t lastActionTime = millis();
-    float ax = 0, ay = 0, az = 0;
+void showCyberBeaverBoot() {
+    M5.Lcd.fillScreen(BLACK);
 
-    while (true) {
-        M5.update();
-        M5.IMU.getAccelData(&ax, &ay, &az);
+    for (int i = 0; i <= 100; i += 5) {
+        drawDetailedBeaver(75, 10, i / 10);
 
-        if (M5.BtnA.wasPressed()) {
-            logsChopped++;
-            if (logsChopped % 5 == 0) boberLevel++;
-            mood = BOBER_CHOP;
-            lastActionTime = millis();
-        }
+        M5.Lcd.fillRect(0, 85, 240, 50, BLACK);
 
-        if (M5.BtnB.wasPressed()) break;
-
-        if (millis() - lastActionTime > 2000) {
-            if (abs(ax) > 0.6 || abs(ay) > 0.6) mood = BOBER_HAPPY;
-            else mood = BOBER_IDLE;
-        }
-
-        M5.Lcd.fillScreen(BLACK);
-
-        M5.Lcd.fillRect(0, 0, 240, 18, NAVY);
-        M5.Lcd.setTextColor(WHITE, NAVY);
+        M5.Lcd.setTextColor(CYAN, BLACK);
         M5.Lcd.setTextSize(1);
-        M5.Lcd.setCursor(5, 5);
-        M5.Lcd.printf("CYBER BOBER | LVL: %d | LOGS: %d", boberLevel, logsChopped);
+        M5.Lcd.setCursor(65, 88);
+        M5.Lcd.println("STICKZERO OS v5.0");
 
-        drawPixelBeaver(85, 26, frame, mood);
-
-        M5.Lcd.fillRect(0, 102, 240, 33, BLACK);
-        M5.Lcd.drawFastHLine(0, 101, 240, DARKCYAN);
-        M5.Lcd.setTextColor(GREEN, BLACK);
-        M5.Lcd.setTextSize(1);
-        M5.Lcd.setCursor(10, 106);
-
-        if (mood == BOBER_CHOP) M5.Lcd.println("Status: CHOPPING WOOD! +1 Log");
-        else if (mood == BOBER_HAPPY) M5.Lcd.println("Status: Wheee! Beaver is tilted!");
-        else M5.Lcd.println("Status: Cyber Beaver is guarding OS");
+        M5.Lcd.drawRect(30, 102, 180, 10, WHITE);
+        M5.Lcd.fillRect(32, 104, (int)(i * 1.76), 6, GREEN);
 
         M5.Lcd.setTextColor(DARKGREY, BLACK);
-        M5.Lcd.setCursor(10, 118);
-        M5.Lcd.println("[A] Feed / Chop Wood    [B] Exit");
+        M5.Lcd.setCursor(85, 118);
+        M5.Lcd.printf("Loading %d%%", i);
 
-        frame++;
-        delay(100);
+        delay(15);
     }
+    delay(150);
 }
