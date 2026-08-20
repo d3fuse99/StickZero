02f6ed7d429b09f19b433ddf94f5c7c310f7d59a
d3fuse99 <d3fuse99@gmail.com>
2026-08-20 20:38:33 +0300

    feat: implement StickZero OS v5.0 and update src/ir_controller.cpp

--- src/ir_controller.cpp
+++ src/ir_controller.cpp
@@ -2,86 +2,51 @@
 
 const uint16_t kIrLedPin = 9;
 IRsend irsend(kIrLedPin, true);
-IRac ac(kIrLedPin, true);
 
 void initIrController() {
     irsend.begin();
 }
 
 void sendSenseiAcPower() {
-    ac.next.protocol = decode_type_t::COOLIX;
-    ac.next.power = true;
-    ac.next.mode = stdAc::opmode_t::kCool;
-    ac.next.degrees = 24;
-    ac.sendAc();
-    delay(60);
-
-    ac.next.protocol = decode_type_t::GREE;
-    ac.sendAc();
-}
-
-void sendTvUniversal() {
-    irsend.sendNEC(0xE0E040BF, 32, 3);
-    delay(30);
-    irsend.sendNEC(0x20DF10EF, 32, 3);
-    delay(30);
-    irsend.sendSony(0xA90, 12, 4);
-    delay(30);
-    irsend.sendPanasonic64(0x40040100BCBD);
-    delay(30);
-    irsend.sendRC5(0x0C, 12, 2);
-    delay(30);
-    irsend.sendRC6(0x1000C, 20, 2);
-    delay(30);
-    irsend.sendNEC(0x02FD48B7, 32, 3);
+    irsend.sendCOOLIX(0xB24D7B, 24, 3);
+    delay(50);
+    irsend.sendGree(0x1100000000000000ULL, 64, 2);
 }
 
 void sendFullTvBGone() {
     irsend.sendNEC(0x807618E7, 32, 3);
-    delay(35);
+    delay(30);
     irsend.sendNEC(0x00FF00FF, 32, 3);
-    delay(35);
+    delay(30);
     irsend.sendNEC(0xE0E040BF, 32, 3);
-    delay(35);
+    delay(30);
     irsend.sendNEC(0x20DF10EF, 32, 3);
-    delay(35);
+    delay(30);
     irsend.sendSony(0xA90, 12, 4);
-    delay(35);
+    delay(30);
     irsend.sendSony(0xA90, 15, 4);
-    delay(35);
+    delay(30);
     irsend.sendPanasonic64(0x40040100BCBD);
-    delay(35);
+    delay(30);
     irsend.sendRC5(0x0C, 12, 2);
-    delay(35);
+    delay(30);
     irsend.sendRC6(0x1000C, 20, 2);
-    delay(35);
+    delay(30);
     irsend.sendNEC(0x02FD48B7, 32, 3);
-    delay(35);
+    delay(30);
     irsend.sendNEC(0x46A802FD, 32, 3);
-    delay(35);
+    delay(30);
     irsend.sendNEC(0x10EF20DF, 32, 3);
 }
 
 void sendAcUniversal() {
-    ac.next.power = true;
-    ac.next.mode = stdAc::opmode_t::kCool;
-    ac.next.degrees = 24;
-
-    decode_type_t acProtocols[] = {
-        decode_type_t::GREE,
-        decode_type_t::COOLIX,
-        decode_type_t::MIDEA,
-        decode_type_t::DAIKIN,
-        decode_type_t::HAIER_AC,
-        decode_type_t::LG2,
-        decode_type_t::SAMSUNG_AC,
-        decode_type_t::PANASONIC_AC,
-        decode_type_t::MITSUBISHI_AC
-    };
-
-    for (int i = 0; i < 9; i++) {
-        ac.next.protocol = acProtocols[i];
-        ac.sendAc();
-        delay(50);
-    }
+    irsend.sendCOOLIX(0xB24D7B, 24, 3);
+    delay(40);
+    irsend.sendCOOLIX(0xB27BE0, 24, 3);
+    delay(40);
+    irsend.sendGree(0x1100000000000000ULL, 64, 2);
+    delay(40);
+    irsend.sendPanasonic64(0x0220E00400000006ULL, 48, 2);
+    delay(40);
+    irsend.sendNEC(0x807618E7, 32, 3);
 }
