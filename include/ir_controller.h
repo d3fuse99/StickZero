50094c7a2851e07b96cdbee00b4f2062918bab15
d3fuse99 <d3fuse99@gmail.com>
2026-08-20 20:38:32 +0300

    feat: implement StickZero OS v5.0 and update include/ir_controller.h

--- include/ir_controller.h
+++ include/ir_controller.h
@@ -1,11 +1,10 @@
 #pragma once
+#include <Arduino.h>
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
-#include <IRac.h>
 
 extern IRsend irsend;
 void initIrController();
-void sendTvUniversal();
-void sendAcUniversal();
 void sendFullTvBGone();
 void sendSenseiAcPower();
+void sendAcUniversal();
