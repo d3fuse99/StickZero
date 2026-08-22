c92735efa3fea4617989abdd65f2d86bf4aa4007
d3fuse99 <d3fuse99@gmail.com>
2026-08-22 13:06:30 +0300

    feat: complete audit fixes, anti-spy matrix keypad in src/flashlight.cpp

--- src/flashlight.cpp
+++ src/flashlight.cpp
@@ -1,71 +1,116 @@
 #include "flashlight.h"
 
-void startDirectFlashlight() {
+void runSolidFlashlight() {
     pinMode(10, OUTPUT);
-    int currentMode = 0;
-
+    digitalWrite(10, LOW);
     M5.Axp.ScreenBreath(100);
+    M5.Lcd.fillScreen(WHITE);
 
+    delay(200);
     while (true) {
-        if (currentMode == 0) {
-            digitalWrite(10, LOW);
-            M5.Lcd.fillScreen(WHITE);
-            M5.Lcd.setTextColor(BLACK, WHITE);
-            M5.Lcd.setTextSize(2);
-            M5.Lcd.setCursor(35, 45);
-            M5.Lcd.println("WHITE LIGHT");
-            M5.Lcd.setTextSize(1);
-            M5.Lcd.setCursor(20, 85);
-            M5.Lcd.println("[B] Next Mode | [A] Exit");
-
-            while (true) {
-                M5.update();
-                if (M5.BtnB.wasPressed()) { currentMode = 1; break; }
-                if (M5.BtnA.wasPressed()) { currentMode = -1; break; }
-                delay(30);
-            }
-        } else if (currentMode == 1) {
-            const int dot = 150;
-            const int dash = 450;
-            int morse[] = {dot, dot, dot, dash, dash, dash, dot, dot, dot};
-
-            for (int i = 0; i < 9; i++) {
-                M5.Lcd.fillScreen(WHITE);
-                digitalWrite(10, LOW);
-                delay(morse[i]);
-                M5.Lcd.fillScreen(BLACK);
-                digitalWrite(10, HIGH);
-                delay(150);
-
-                M5.update();
-                if (M5.BtnB.wasPressed()) { currentMode = 2; break; }
-                if (M5.BtnA.wasPressed()) { currentMode = -1; break; }
-            }
-            delay(500);
-        } else if (currentMode == 2) {
-            for (int i = 0; i < 3; i++) {
-                M5.Lcd.fillScreen(RED);
-                digitalWrite(10, LOW);
-                delay(60);
-                M5.Lcd.fillScreen(BLACK);
-                digitalWrite(10, HIGH);
-                delay(60);
-            }
-            for (int i = 0; i < 3; i++) {
-                M5.Lcd.fillScreen(BLUE);
-                delay(60);
-                M5.Lcd.fillScreen(BLACK);
-                delay(60);
-            }
-
-            M5.update();
-            if (M5.BtnB.wasPressed()) { currentMode = 0; }
-            if (M5.BtnA.wasPressed()) { currentMode = -1; }
-        }
-
-        if (currentMode == -1) break;
+        M5.update();
+        if (M5.BtnB.wasPressed() || M5.BtnA.wasPressed()) break;
+        delay(20);
     }
 
     digitalWrite(10, HIGH);
-    M5.Axp.ScreenBreath(25);
+    M5.Axp.ScreenBreath(18);
+}
+
+void runSosStrobe() {
+    pinMode(10, OUTPUT);
+    M5.Axp.ScreenBreath(100);
+
+    const int dot = 120;
+    const int dash = 360;
+    int morse[] = {dot, dot, dot, dash, dash, dash, dot, dot, dot};
+
+    while (true) {
+        for (int i = 0; i < 9; i++) {
+            M5.Lcd.fillScreen(WHITE);
+            digitalWrite(10, LOW);
+
+            for (int t = 0; t < morse[i]; t += 20) {
+                M5.update();
+                if (M5.BtnB.wasPressed() || M5.BtnA.wasPressed()) {
+                    digitalWrite(10, HIGH);
+                    M5.Axp.ScreenBreath(18);
+                    return;
+                }
+                delay(20);
+            }
+
+            M5.Lcd.fillScreen(BLACK);
+            digitalWrite(10, HIGH);
+
+            for (int t = 0; t < 120; t += 20) {
+                M5.update();
+                if (M5.BtnB.wasPressed() || M5.BtnA.wasPressed()) {
+                    M5.Axp.ScreenBreath(18);
+                    return;
+                }
+                delay(20);
+            }
+        }
+    }
+}
+
+void runPoliceFlasher() {
+    M5.Axp.ScreenBreath(100);
+    pinMode(10, OUTPUT);
+
+    while (true) {
+        for (int i = 0; i < 3; i++) {
+            M5.Lcd.fillScreen(RED);
+            digitalWrite(10, LOW);
+            for (int t = 0; t < 50; t += 10) {
+                M5.update();
+                if (M5.BtnB.wasPressed() || M5.BtnA.wasPressed()) goto exit_police;
+                delay(10);
+            }
+            M5.Lcd.fillScreen(BLACK);
+            digitalWrite(10, HIGH);
+            for (int t = 0; t < 50; t += 10) {
+                M5.update();
+                if (M5.BtnB.wasPressed() || M5.BtnA.wasPressed()) goto exit_police;
+                delay(10);
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            M5.Lcd.fillScreen(BLUE);
+            for (int t = 0; t < 50; t += 10) {
+                M5.update();
+                if (M5.BtnB.wasPressed() || M5.BtnA.wasPressed()) goto exit_police;
+                delay(10);
+            }
+            M5.Lcd.fillScreen(BLACK);
+            for (int t = 0; t < 50; t += 10) {
+                M5.update();
+                if (M5.BtnB.wasPressed() || M5.BtnA.wasPressed()) goto exit_police;
+                delay(10);
+            }
+        }
+    }
+
+exit_police:
+    digitalWrite(10, HIGH);
+    M5.Axp.ScreenBreath(18);
+}
+
+void startDirectFlashlight() {
+    int currentMode = 0;
+
+    while (true) {
+        if (currentMode == 0) {
+            runSolidFlashlight();
+            currentMode = 1;
+        } else if (currentMode == 1) {
+            runSosStrobe();
+            currentMode = 2;
+        } else if (currentMode == 2) {
+            runPoliceFlasher();
+            break;
+        }
+    }
 }
