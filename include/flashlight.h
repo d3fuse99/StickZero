dd54fa7e82be1193e7375f9fc338b97768e9147c
d3fuse <d3fuse99@gmail.com>
2026-08-25 16:52:11 +0300

    Delete include directory

--- include/flashlight.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <M5StickCPlus.h>
-
-void startDirectFlashlight();
--- include/radar.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <M5StickCPlus.h>
-#include <BLEDevice.h>
-#include <BLEScan.h>
-#include <BLEAdvertisedDevice.h>
-
-void startBleRadar();
